14d3369b98906985df70566d11db16fee7486c91
Thurian <thuriansacristan@gmail.com>
2025-04-01 16:36:39 +0200

    ajout LaticeApplicationConsole

---
+++ src/main/java/latice/application/LaticeApplicationConsole.java
@@ -0,0 +1,11 @@
+package latice.application;
+
+public class LaticeApplicationConsole {
+
+	public static void main(String[] args) {
+		System.out.println("-----------------------------------------------");
+		System.out.println("Bienvenue dans notre magnifique jeu de latice !");
+		System.out.println("-----------------------------------------------");
+	}
+
+}
