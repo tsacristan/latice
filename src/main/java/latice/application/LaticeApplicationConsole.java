bb04af10096b5c9293a9780773eab2bb597ae083
Alexandre Lechault <lechaultalexandre05@gmail.com>
2025-04-01 16:59:16 +0200

    Message pour le collaborateur n°2

--- src/main/java/latice/application/LaticeApplicationConsole.java
+++ src/main/java/latice/application/LaticeApplicationConsole.java
@@ -8,6 +8,8 @@ public class LaticeApplicationConsole {
 		System.out.println("-----------------------------------------------");
 		System.out.println("Développé par Hugo");
 		System.out.println("-----------------------------------------------");
+		System.out.println("Développé par Alexandre");
+		System.out.println("-----------------------------------------------");
 	}
 
 }
