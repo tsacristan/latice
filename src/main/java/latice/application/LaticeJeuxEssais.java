acc320ecd7b04f82763890a062cd3a73ff53e3bf
flastar <hgbequet@gmail.com>
2025-04-07 10:21:23 +0200

    ajout du rack joueur

--- src/main/java/latice/application/LaticeJeuxEssais.java
+++ src/main/java/latice/application/LaticeJeuxEssais.java
@@ -1,22 +1,26 @@
 package latice.application;
 
-import latice.model.LaticePile;
+import latice.model.PileDebut;
+import latice.model.PileJoueur;
+import latice.model.RackJoueur;
 
 public class LaticeJeuxEssais {
 
 	public static void main(String[] args) {
-		LaticePile pile = new LaticePile();
-		pile.remplirPile();
-		pile.melangerPile();
+		PileDebut pile = new PileDebut();
+		pile.remplir();
+		pile.melanger();
 		System.out.println(pile);
 		System.out.println(pile.size());
 
-		LaticePile pile1 = new LaticePile();
-		LaticePile pile2 = new LaticePile();
-		pile.distribuer(new LaticePile[]{pile1, pile2});
+		PileJoueur pile1 = new PileJoueur();
+		PileJoueur pile2 = new PileJoueur();
+		pile.distribuer(new PileJoueur[]{pile1, pile2});
 		System.out.println(pile1);
 		System.out.println(pile2);
 
+		RackJoueur rackJoueur = new RackJoueur();
+		rackJoueur.remplir(pile1);
+		System.out.println(rackJoueur);
 	}
-
 }
---
+++ src/main/java/latice/model/RackJoueur.java
@@ -0,0 +1,26 @@
+package latice.model;
+
+import java.util.Arrays;
+
+public class RackJoueur {
+    private final Tuile[] rack;
+
+    public RackJoueur(Tuile[] rack) {
+        this.rack = rack;
+    }
+
+    public RackJoueur() {
+        this(new Tuile[5]);
+    }
+
+    public void remplir(PileJoueur pile) {
+        for (int i = 0; i < 5; i++) {
+            rack[i] = pile.retirerTuile();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(rack);
+    }
+}
