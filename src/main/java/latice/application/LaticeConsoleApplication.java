d366bab09f6ac1703f24cc968e3b1014849d6037
flastar <hgbequet@gmail.com>
2025-04-07 08:36:08 +0200

    modification noms

--- src/main/java/latice/application/LaticeConsoleApplication.java
+++ src/main/java/latice/application/LaticeConsoleApplication.java
@@ -1,6 +1,6 @@
 package latice.application;
 
-public class LaticeApplicationConsole {
+public class LaticeConsoleApplication {
 
 	public static void main(String[] args) {
 		System.out.println("-----------------------------------------------");
--- src/main/java/latice/model/LaticePille.java
+++
@@ -1,5 +0,0 @@
-package latice.model;
-
-public class LaticePille {
-
-}
