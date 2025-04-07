7d2b5c885b19154bc4147f64ad89162832859a6a
hugob <hugo.bequet@etu.unilim.fr>
2025-04-07 08:44:13 +0200

    ajout énumérations Forme Couleur

---
+++ src/main/java/latice/model/Couleur.java
@@ -0,0 +1,10 @@
+package latice.model;
+
+public enum Couleur {
+    JAUNE,
+    BLEU_MARINE,
+    MAGENTA,
+    ROUGE,
+    VERT,
+    BLEU_SARCELLE
+}
---
+++ src/main/java/latice/model/Forme.java
@@ -0,0 +1,10 @@
+package latice.model;
+
+public enum Forme {
+    PLUME,
+    OISEAU,
+    TORTUE,
+    FLEUR,
+    GECKO,
+    DAUPHIN
+}
