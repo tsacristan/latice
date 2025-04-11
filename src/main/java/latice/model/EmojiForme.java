c6eee245106625ac2237e8dfd6ea736e55b001a9
Alexandre Lechault <lechaultalexandre05@gmail.com>
2025-04-11 15:01:39 +0200

    ajout EmojiForme.java

---
+++ src/main/java/latice/model/EmojiForme.java
@@ -0,0 +1,25 @@
+package latice.model;
+
+public enum EmojiForme {
+	PLUME("\uD83E\uDEB6"),
+	OISEAU("\uD83D\uDC26"),
+	TORTUE("\uD83D\uDC22"),
+	FLEUR("\uD83C\uDF3C"),
+	GECKO("\uD83E\uDD8E"),
+	DAUPHIN("\uD83D\uDC2C");
+	
+	private final String emoji;
+
+	EmojiForme(String emoji) {
+    	this.emoji = emoji;
+	}
+
+	public String Emoji() {
+		return emoji;
+	}
+
+	@Override
+	public String toString() {
+    	return emoji + " (" + name() + ")";
+	}
+}
