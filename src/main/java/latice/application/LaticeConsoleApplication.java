d0c9339f21b8a8ee53b971584502dd12518ae909
hugob <hugo.bequet@etu.unilim.fr>
2025-04-11 13:53:19 +0200

    ajout LaticeConsoleApplication avec les tests

--- src/main/java/latice/application/LaticeConsoleApplication.java
+++ src/main/java/latice/application/LaticeConsoleApplication.java
@@ -1,19 +1,23 @@
 package latice.application;
 
+import latice.model.PileDebut;
+import latice.model.PileJoueur;
+import latice.model.RackJoueur;
+
 public class LaticeConsoleApplication {
 
 	public static void main(String[] args) {
-		System.out.println("-----------------------------------------------");
-		System.out.println("Bienvenue dans notre magnifique jeu de latice !");
-		System.out.println("-----------------------------------------------");
-		System.out.println("Développé par Hugo");
-		System.out.println("-----------------------------------------------");
-		System.out.println("Développé par Alexandre");
-		System.out.println("-----------------------------------------------");
-		System.out.println("Développé par Rémi");
-		System.out.println("-----------------------------------------------");
-		System.out.println("Développé par Thurian");
-		System.out.println("-----------------------------------------------");
-	}
+		PileDebut pile = new PileDebut();
+		pile.remplir();
+		pile.melanger();
 
+		PileJoueur pile1 = new PileJoueur();
+		PileJoueur pile2 = new PileJoueur();
+		pile.distribuer(new PileJoueur[]{pile1, pile2});
+		
+		RackJoueur rackJoueur = new RackJoueur();
+		rackJoueur.remplir(pile1);
+		System.out.println(rackJoueur);
+	}
+	
 }
