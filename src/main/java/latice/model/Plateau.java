cbb385bf4ea73aff44f52bb68dee649343e9abca
Rémi Bastide <remi.bastide29@gmail.com>
2025-04-17 09:53:10 +0200

    Modification de Plateau et ajout des test de Plateau

--- src/main/java/latice/model/Plateau.java
+++ src/main/java/latice/model/Plateau.java
@@ -2,31 +2,51 @@ package latice.model;
 
 public class Plateau {
     private final Case[][] grille;
-    private final int taille = 9;
-
+    private final int colonnes = 9;
+    private final int lignes = 9;
+    		
     public Plateau() {
-        grille = new Case[taille][taille];
+        grille = new Case[lignes][colonnes];
 
 
-        for (int i = 0; i < taille; i++) {
-            for (int j = 0; j < taille; j++) {
-                grille[i][j] = new Case();
+        for (int i = 0; i < lignes; i++) {
+            for (int j = 0; j < colonnes; j++) {
+                grille[i][j] = new Case(TypeCase.CASE_VIDE);
             }
         }
 
-
         grille[4][4] = new Case(TypeCase.CASE_LUNE);
+        
+        grille[0][0] = new Case(TypeCase.CASE_SOLEIL);
+        grille[1][1] = new Case(TypeCase.CASE_SOLEIL);
         grille[2][2] = new Case(TypeCase.CASE_SOLEIL);
+        
+        grille[0][4] = new Case(TypeCase.CASE_SOLEIL);
+        
+        grille[0][8] = new Case(TypeCase.CASE_SOLEIL);
+        grille[1][7] = new Case(TypeCase.CASE_SOLEIL);
         grille[2][6] = new Case(TypeCase.CASE_SOLEIL);
+        
+        grille[4][0] = new Case(TypeCase.CASE_SOLEIL);
+        
+        grille[8][0] = new Case(TypeCase.CASE_SOLEIL);
+        grille[7][1] = new Case(TypeCase.CASE_SOLEIL);
         grille[6][2] = new Case(TypeCase.CASE_SOLEIL);
+        
+        grille[8][4] = new Case(TypeCase.CASE_SOLEIL);
+        
+        grille[8][8] = new Case(TypeCase.CASE_SOLEIL);
+        grille[7][7] = new Case(TypeCase.CASE_SOLEIL);
         grille[6][6] = new Case(TypeCase.CASE_SOLEIL);
+        
+        grille[4][8] = new Case(TypeCase.CASE_SOLEIL);
     }
 
     public Case[][] grille() {
         return grille;
     }
 
-    public int taille() {
-        return taille;
+    public int tailleDuTableau() {
+        return lignes * colonnes;
     }
 }
--- src/test/java/latice/TestLatice.java
+++ src/test/java/latice/TestLatice.java
@@ -4,8 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import latice.model.Case;
 import latice.model.PileDebut;
 import latice.model.PileJoueur;
+import latice.model.Plateau;
+import latice.model.TypeCase;
 
 class TestLatice {
 
@@ -41,4 +44,37 @@ class TestLatice {
 		assertEquals(pile1.size(),pile2.size());
 	}
 
+	@Test
+	void TestTaillePlateau() {
+		Plateau plateau = new Plateau();
+		assertEquals(81,plateau.tailleDuTableau());
+	}
+	
+	@Test
+	void TestCaseLuneDuPlateau() {
+		Plateau plateau = new Plateau();
+		TypeCase lune = TypeCase.CASE_LUNE;
+		assertTrue((plateau.grille()[4][4].typeCase() == lune));
+	}
+	
+	void TestDesCasesSoleilsDuPlateau() {
+		Plateau plateau = new Plateau();
+		TypeCase soleil = TypeCase.CASE_SOLEIL;
+		assertTrue((plateau.grille()[0][0].typeCase() == soleil));
+		assertTrue((plateau.grille()[1][1].typeCase() == soleil));
+		assertTrue((plateau.grille()[2][2].typeCase() == soleil));
+		assertTrue((plateau.grille()[0][4].typeCase() == soleil));
+		assertTrue((plateau.grille()[0][8].typeCase() == soleil));
+		assertTrue((plateau.grille()[1][7].typeCase() == soleil));
+		assertTrue((plateau.grille()[2][6].typeCase() == soleil));
+		assertTrue((plateau.grille()[4][0].typeCase() == soleil));
+		assertTrue((plateau.grille()[8][0].typeCase() == soleil));
+		assertTrue((plateau.grille()[7][1].typeCase() == soleil));
+		assertTrue((plateau.grille()[6][2].typeCase() == soleil));
+		assertTrue((plateau.grille()[8][4].typeCase() == soleil));
+		assertTrue((plateau.grille()[8][8].typeCase() == soleil));
+		assertTrue((plateau.grille()[7][7].typeCase() == soleil));
+		assertTrue((plateau.grille()[6][6].typeCase() == soleil));
+		assertTrue((plateau.grille()[4][8].typeCase() == soleil));
+	}
 }
