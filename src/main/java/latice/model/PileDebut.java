e15ad6522a8d9890a856d764081ff598cb46c10d
hugob <hgbequet@gmail.com>
2025-05-06 11:02:47 +0200

    Refacto magic number de la PileDebut

--- src/main/java/latice/model/PileDebut.java
+++ src/main/java/latice/model/PileDebut.java
@@ -3,6 +3,8 @@ package latice.model;
 import java.util.Collections;
 
 public class PileDebut extends LaticePile {
+	private final int nombreTuilesTotal = 72;
+	
     public void remplir() {
         for (Couleur couleur : Couleur.values()) {
             for (Forme forme : Forme.values()) {
@@ -18,7 +20,7 @@ public class PileDebut extends LaticePile {
 
     public void distribuer(PileJoueur[] pilesJoueur) {
         for (LaticePile pileJoueur : pilesJoueur) {
-            for (int i = 0; i < 36; i++) {
+            for (int i = 0; i < nombreTuilesTotal / pilesJoueur.length; i++) {
                 pileJoueur.ajouterTuile(retirerTuile());
             }
         }
