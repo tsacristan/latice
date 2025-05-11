057b56a6cabc3713292473184b07f804209f0a59
hugob <hgbequet@gmail.com>
2025-05-11 16:10:56 +0200

    Ajout de la partie du haut de l'interface

---
+++ src/main/java/latice/view/PartieHaut.java
@@ -0,0 +1,35 @@
+ 
+package latice.view;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+
+public class PartieHaut extends HBox {
+	
+	private static final String STYLE_COMMUN = "-fx-font-size: 30px; -fx-alignment: center;";
+    
+    public PartieHaut() {
+        Label lblHeure = new Label("00:00");
+        Label lblTour = new Label("Tour :");
+        Button btnAide = new Button("Règles");
+        
+        lblHeure.setStyle(STYLE_COMMUN);
+        lblTour.setStyle(STYLE_COMMUN);
+        btnAide.setStyle(STYLE_COMMUN);
+        
+        
+        lblTour.setMaxWidth(Double.MAX_VALUE);
+        
+        setPadding(new Insets(10, 20, 10, 20));
+        
+        HBox.setHgrow(lblTour, Priority.ALWAYS);
+        HBox.setHgrow(lblHeure, Priority.ALWAYS);
+        HBox.setHgrow(btnAide, Priority.ALWAYS);
+
+        getChildren().addAll(lblHeure,lblTour,btnAide);
+    }
+    
+}
