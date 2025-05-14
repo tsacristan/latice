c5c64487fb34b3598ef15ae597eda3005c29546a
hugob <hgbequet@gmail.com>
2025-05-14 11:09:22 +0200

    Ajout de la classe Joueur

---
+++ src/main/java/latice/model/Joueur.java
@@ -0,0 +1,35 @@
+package latice.model;
+
+public class Joueur {
+	
+	private String pseudo;
+	private RackJoueur rackJoueur;
+	private PileJoueur pileJoueur;
+	
+	public Joueur(String pseudo, RackJoueur rackJoueur, PileJoueur pileJoueur) {
+		this.pseudo = pseudo;
+		this.rackJoueur = rackJoueur;
+		this.pileJoueur = pileJoueur;
+	}
+	
+	public Joueur(String pseudo) {
+		this(pseudo, new RackJoueur(), new PileJoueur());
+	}
+	
+	public Joueur() {
+		this("Défaut", new RackJoueur(), new PileJoueur());
+	}
+	
+	public String pseudo() {
+		return pseudo;
+	}
+	
+	public RackJoueur rackJoueur() {
+		return rackJoueur;
+	}
+	
+	public PileJoueur pileJoueur() {
+		return pileJoueur;
+	}
+
+}
