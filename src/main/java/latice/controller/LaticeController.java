3b219fbd7dab9588b155579b6b7fa856ee2f7cac
Rémi Bastide <remi.bastide29@gmail.com>
2025-05-14 11:43:09 +0200

    Modificiation de LaticeController

---
+++ src/main/java/latice/controller/LaticeController.java
@@ -0,0 +1,48 @@
+package latice.controller;
+
+import java.util.Random;
+import java.util.Scanner;
+
+import latice.ihm.Console;
+import latice.model.Joueur;
+import latice.model.PileDebut;
+import latice.model.PileJoueur;
+import latice.model.RackJoueur;
+import latice.util.PiocheInvalideException;
+
+public class LaticeController {
+	
+	public static void demarrerJeu() throws PiocheInvalideException {
+		Random random = new Random();
+		
+		Scanner choix = new Scanner(System.in);
+		
+		//Choix des pseudos
+		
+		Console.message("Veuillir choisir le pseudo du joueur 1 : ");
+		String reponse1 = choix.nextLine();
+		Joueur joueur1 = new Joueur(reponse1);
+		
+		Console.message("Veuillir choisir le pseudo du joueur 2 : ");
+		String reponse2 = choix.nextLine();
+		Joueur joueur2 = new Joueur(reponse2);
+		
+		PileDebut pile = new PileDebut();
+		pile.remplir();
+		
+		pile.distribuer(new PileJoueur[]{joueur1.pileJoueur(),joueur2.pileJoueur()});
+		
+		joueur1.rackJoueur().remplir();
+		joueur2.rackJoueur().remplir();
+		
+		Joueur[] joueurs = {joueur1,joueur2};
+		
+		int joueurActif = random.nextBoolean() ? 0 : 1;
+		
+		Console.message("Le premier joueur est " + joueurs[joueurActif]);
+		
+		Console.message(joueurs[joueurActif].rackJoueur().toString());
+		
+		
+	}
+}
