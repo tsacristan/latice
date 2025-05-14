7abb413dd7c1cd83425b440971405883b7edeaa0
hugob <hgbequet@gmail.com>
2025-05-14 08:34:39 +0200

    Implémentation tests unitaires et equals de Case, Tuile et LaticePile

--- src/test/java/latice/TestLatice.java
+++ src/test/java/latice/TestLatice.java
@@ -16,38 +16,6 @@ import latice.model.TypeCase;
 class TestLatice {
 
 	@Test
-	void TestTaillePileDebutEgaleA72() {
-		PileDebut pile = new PileDebut();
-		pile.remplir();
-		
-		assertEquals(72,pile.size());
-	}
-	
-	@Test
-	void TestSiLaPileEstMelanger() {
-		PileDebut pile = new PileDebut();
-		PileDebut pile2 = new PileDebut();
-		pile.remplir();
-		pile2.remplir();
-		pile2.melanger();
-		
-		assertNotEquals(pile,pile2);
-	}
-	
-	@Test
-	void TestTailleDesPilesJoueursEgales() {
-		PileDebut pile = new PileDebut();
-		pile.remplir();
-		
-		
-		PileJoueur pile1 = new PileJoueur();
-		PileJoueur pile2 = new PileJoueur();
-		pile.distribuer(new PileJoueur[]{pile1, pile2});
-		
-		assertEquals(pile1.size(),pile2.size());
-	}
-
-	@Test
 	void TestTaillePlateau() {
 		Plateau plateau = new Plateau();
 		assertEquals(81,plateau.tailleDuTableau());
@@ -79,37 +47,5 @@ class TestLatice {
 		assertTrue((plateau.grille()[7][7].typeCase() == soleil));
 		assertTrue((plateau.grille()[6][6].typeCase() == soleil));
 		assertTrue((plateau.grille()[4][8].typeCase() == soleil));
-	}
-	
-	// Tuiles 
-	@Test
-	void TestCouleurEtFormeTuile() {
-		Tuile tuile = new Tuile(Couleur.BLEU_MARINE,Forme.DAUPHIN);
-		assertTrue(tuile.couleur() == Couleur.BLEU_MARINE);
-		assertTrue(tuile.forme() == Forme.DAUPHIN);
-		}
-	
-	
-	//Case
-	@Test
-	void  TestTuileExiste() {
-		Tuile tuile = new Tuile(Couleur.JAUNE,Forme.GECKO);
-		Case bloc = new Case(tuile,TypeCase.CASE_SOLEIL);
-		assertTrue(tuile.equals(bloc.tuile()));
-	}
-	@Test
-	void  TestChangementTuile() {
-		Tuile tuile = new Tuile(Couleur.JAUNE,Forme.GECKO);
-		Tuile tuile2 = new Tuile(Couleur.BLEU_MARINE,Forme.DAUPHIN);
-		Case bloc = new Case(tuile,TypeCase.CASE_SOLEIL);
-		bloc.changerTuile(tuile2);
-		assertTrue(tuile2.equals(bloc.tuile()));
-	}
-	@Test
-	void  TestTypeCase() {
-		Tuile tuile = new Tuile(Couleur.JAUNE,Forme.GECKO);
-		Case bloc = new Case(tuile,TypeCase.CASE_SOLEIL);
-		bloc.changerTypeCase(TypeCase.CASE_LUNE);
-		assertTrue(bloc.typeCase() == TypeCase.CASE_LUNE);
 	}
 }
---
+++ src/test/java/latice/model/TestPileDebut.java
@@ -0,0 +1,44 @@
+package latice.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+
+class TestPileDebut {
+	
+	private static final int TAILLE_PILE_DEBUT = 72;
+	
+	@Test
+	void test_taile_pile_debut_egale_a_72() {
+		PileDebut pile = new PileDebut();
+		pile.remplir();
+		
+		assertEquals(TAILLE_PILE_DEBUT, pile.size());
+	}
+	
+	@Test
+	void test_si_la_pile_est_melangee() {
+		PileDebut pile = new PileDebut();
+		PileDebut pile2 = new PileDebut();
+		pile.remplir();
+		pile2.remplir();
+		pile2.melanger();
+		
+		assertNotEquals(pile, pile2);
+	}
+
+	@Test
+	void test_distribution_taille_des_piles_joueurs_egales() {
+		PileDebut pile = new PileDebut();
+		pile.remplir();
+		
+		
+		PileJoueur pile1 = new PileJoueur();
+		PileJoueur pile2 = new PileJoueur();
+		pile.distribuer(new PileJoueur[]{pile1, pile2});
+		
+		assertEquals(pile1.size(), pile2.size());
+	}
+
+}
