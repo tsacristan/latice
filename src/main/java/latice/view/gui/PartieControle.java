637ed3adf14637f3db21f19bdc1efac291841830
Thurian <thuriansacristan@gmail.com>
2025-05-28 18:39:45 +0200

    Rack centré

--- src/main/java/latice/view/gui/PartieControle.java
+++ src/main/java/latice/view/gui/PartieControle.java
@@ -3,41 +3,40 @@ package latice.view.gui;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import latice.model.player.RackJoueur;
 
 public class PartieControle extends HBox {
 	
-	private GridPane tableauBas;
 	private InterfaceRack interfaceRack;
 	private static final String STYLE_COMMUN = "-fx-font-size: 25px; -fx-alignment: center;";
 
 	public PartieControle(RackJoueur rackJoueur) {
-		placerBoutons();
-		
-		interfaceRack = new InterfaceRack(rackJoueur);
-		setPadding(new Insets(0,20,0,0));
-		setAlignment(Pos.CENTER);
-        tableauBas.add(interfaceRack, 1, 0);
-	}
-	
-	private void placerBoutons() {
-		Button valider = new Button("Valider");
-        Button passer = new Button("Passer");
-        Button piocher = new Button("Piocher");
-        tableauBas = new GridPane();
+	    Button valider = new Button("Valider");
+	    Button passer = new Button("Passer");
+	    Button piocher = new Button("Piocher");
+	    
+	    
+	    valider.setStyle(STYLE_COMMUN);
+	    passer.setStyle(STYLE_COMMUN);
+	    piocher.setStyle(STYLE_COMMUN);
 
-        tableauBas.setHgap(400);
-        tableauBas.setPadding(new Insets(10, 20, 10, 100));
-        valider.setStyle(STYLE_COMMUN);
-        passer.setStyle(STYLE_COMMUN);
-        piocher.setStyle(STYLE_COMMUN);
-        
-        getChildren().add(tableauBas);
-        tableauBas.add(passer, 0,0);
-        tableauBas.add(piocher, 0,1);
-        tableauBas.add(valider, 2,0);
+	    
+	    VBox vboxBoutonsGauche = new VBox(10); 
+	    vboxBoutonsGauche.getChildren().addAll(passer, piocher);
+	    vboxBoutonsGauche.setAlignment(Pos.CENTER_LEFT);
+	    vboxBoutonsGauche.setPadding(new Insets(0, 20, 0, 20));
+
+	    
+	    interfaceRack = new InterfaceRack(rackJoueur);
+
+	   
+	    setSpacing(50);  
+	    setPadding(new Insets(10, 50, 10, 20));
+	    setAlignment(Pos.CENTER);
+	    
+	    getChildren().addAll(vboxBoutonsGauche, interfaceRack, valider);
 	}
 	
 	public InterfaceRack interfaceRack() {
