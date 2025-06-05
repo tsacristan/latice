3515fc610f0c167e75e5f18fc4518a180fc9d510
hugob <hugo.bequet@etu.unilim.fr>
2025-06-05 15:51:16 +0200

    Suppression des imports inutiles dans LaticeGraphique

--- src/main/java/latice/application/LaticeGraphique.java
+++ src/main/java/latice/application/LaticeGraphique.java
@@ -1,22 +1,11 @@
 package latice.application;
 
 
-import java.io.File;
-
 import javafx.application.Application;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.StackPane;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import latice.controller.ControllerJouerGraphique;
@@ -26,11 +15,10 @@ import latice.view.gui.LaticeVueGraphique;
 
 public class LaticeGraphique extends Application { 
     
-    
     @Override
     public void start(Stage primaryStage) {
         LaticeVueGraphique laticeVue = new LaticeVueGraphique();
-        InterfaceJeu racine = laticeVue.interfaceJeu();
+        InterfaceJeu interfaceJeu = laticeVue.interfaceJeu();
 
         Image image = new Image(getClass().getResource("/image2.png").toExternalForm());
         ImageView backgroundView = new ImageView(image);
@@ -38,10 +26,10 @@ public class LaticeGraphique extends Application {
         backgroundView.setFitWidth(1920);
         backgroundView.setFitHeight(1080);
 
-        StackPane root = new StackPane();
-        root.getChildren().addAll(backgroundView, racine);
+        StackPane racine = new StackPane();
+        racine.getChildren().addAll(backgroundView, interfaceJeu);
 
-        Scene scene = new Scene(root, 1920, 1080); // ✅ Utilise root ici
+        Scene scene = new Scene(racine, 1920, 1080);
 
         scene.getStylesheets().add(getClass().getResource("/CSSTHEME.css").toExternalForm());
 
